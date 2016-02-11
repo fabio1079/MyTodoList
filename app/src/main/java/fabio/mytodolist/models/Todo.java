e6abeddf57981fc80a1ab021a64bdb79eb4dfdba
Fabio Teixeira <fabio1079@gmail.com>
2016-02-10 22:26:53 -0200

    Add Todo model
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>

---
+++ app/src/main/java/fabio/mytodolist/models/Todo.java
@@ -0,0 +1,36 @@
+package fabio.mytodolist.models;
+
+
+public class Todo {
+    public final String TEXT_ERROR_WHEN_EMPTY = "The given text is empty";
+    public final String TEXT_ERROR_WHEN_TOO_BIG = "The given text is bugger than 60 characters";
+
+    private String text;
+    private boolean done;
+
+    public Todo(final String text) {
+        this.setText(text);
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(final String text) throws Error {
+        if (text.isEmpty()) {
+            throw new Error(TEXT_ERROR_WHEN_EMPTY);
+        } else if (text.length() > 60) {
+            throw new Error(TEXT_ERROR_WHEN_TOO_BIG);
+        } else {
+            this.text = text;
+        }
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(final boolean done) {
+        this.done = done;
+    }
+}
